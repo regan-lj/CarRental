7967fa388f4f15450a9f493d1f2082e2d0dfe95b
Regan James <jamre937@student.otago.ac.nz>
2020-09-27 21:56:55 +1300

    Functional cancel button for admin

--- layout/admin.php
+++ layout/admin.php
@@ -10,12 +10,45 @@ include('../hidden/header.php');
 ?>
 <body>
 
+<?php
+if (isset($_POST['cancel'])) {
+
+    // Get the json from bookings.json
+    $input_filename = "../json/bookings.json";
+    $output_filename = "../json/bookings.json";
+    $json_input = file_get_contents($input_filename);
+    $json = json_decode($json_input, true);
+
+    // Identify index of booking and update the json
+    $index = $_POST['index'];
+    // Get all bookings before cancelled booking
+    $updated_bookings = array_slice($json["bookings"]["booking"], 0, $index);
+    // Add the bookings after cancelled booking
+    $after = array_slice($json["bookings"]["booking"], $index+1);
+    foreach ($after as $booking) {
+        array_push($updated_bookings, $booking);
+    }
+
+    // Make the new json structure
+    $new_booking = array("booking" => $updated_bookings);
+    $new_bookings = array("bookings" => $new_booking);
+
+    // Put back the new json
+    $newJsonString = json_encode($new_bookings, JSON_PRETTY_PRINT)."\n";
+    file_put_contents($output_filename, $newJsonString);
+
+    // Notify admin that booking has been cancelled
+    echo "<br><p class='center'>Booking successfully cancelled</p>";
+}
+?>
+
 <br><h2 class="center">Admin</h2>
 
 <?php
 $input_filename = "../json/bookings.json";
 $json_input = file_get_contents($input_filename);
 $json = json_decode($json_input,true);
+$index = 0; // Keep track of index
 
 foreach ($json["bookings"]["booking"] as $booking) {
     $name = $booking["name"];
@@ -30,12 +63,21 @@ foreach ($json["bookings"]["booking"] as $booking) {
     $d_month = $dropoff["month"];
     $d_year = $dropoff["year"];
 
-    echo "<div id='custViewBookings' class=\"car-info\">";
+    // Display the booking information
+    echo "<div class='car-info'>";
     echo "<h3>$rego</h3>";
     echo "<p>$name</p>";
     echo "<p>Pick Up: $p_day-$p_month-$p_year</p>";
-    echo "<p>Drop Off: $d_day-$d_month-$d_year</p>";
+    echo "<p>Drop Off: $d_day-$d_month-$d_year</p><br>";
+
+    // Create a hidden form for cancellation
+    echo "<form method='post'>";
+    echo "<input type='hidden' value=$index name='index'>";
+    echo "<input type='submit' name='cancel' class='cancelButton' value='Cancel'>";
+    echo "</form>";
     echo "</div>";
+
+    $index += 1;
 }
 ?>
 
